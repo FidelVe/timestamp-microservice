4003ecbe0265ef165f35defd3af13ec2820ae134
Fidel Sanchez <fidel@espanicon.team>
2022-03-07 22:37:39 -0400

    Finish microservice

--- server.js
+++ server.js
@@ -6,67 +6,6 @@ require("dotenv").config();
 var express = require("express");
 var app = express();
 
-// functions
-function validateDate(year, month, day) {
-  let isValid = false;
-  const shortMonths = ["02", "04", "06", "09", "11", "2", "4", "6", "9"];
-  const longMonths = ["01", "03", "05", "07", "10", "12", "1", "3", "5", "7"];
-
-  if (month > 12) {
-    // not a valid month
-    // isValid stays equal to 'false'
-  } else {
-    if (shortMonths.includes(month)) {
-      // if the month has at most 30 days
-      if (day < 31) {
-        isValid = true;
-      } else {
-        // isValid stays equal to 'false'
-      }
-    } else if (longMonths.includes(month)) {
-      // if the month has at most 31 days
-      if (day < 32) {
-        isValid = true;
-      } else {
-        // isValid stays equal to 'false'
-      }
-    } else {
-      // error. should never happen
-    }
-  }
-  return isValid;
-}
-function validateDateRoute(path) {
-  const pathArray = path.split("/");
-  const dateString = pathArray[pathArray.length - 1];
-  const dateArray = dateString.split("-");
-  let dateObj = {
-    year: null,
-    month: null,
-    day: null,
-    unix: null,
-    error: null
-  };
-
-  if (dateArray.length === 3) {
-    dateObj.year = dateArray[0];
-    dateObj.month = dateArray[1];
-    dateObj.day = dateArray[2];
-
-    if (validateDate(dateObj.year, dateObj.month, dateObj.day) === true) {
-      // the dates are valid
-    } else {
-      // the date is not valid
-      dateObj.error = "Invalid Date";
-    }
-  } else if (dateArray.length === 1) {
-    dateObj.unix = parseInt(dateArray[0]);
-  } else {
-    // error
-  }
-  return dateObj;
-}
-
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require("cors");
@@ -80,38 +19,28 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// hostname/api/YEAR-MONTH-DAY
-app.get(/^\/api\/\d{4}-\d{1,2}-\d{1,2}\/?$/, (req, res) => {
-  const dateInPath = validateDateRoute(req.path);
-  const date = new Date(
-    dateInPath.year,
-    dateInPath.month - 1,
-    dateInPath.day,
-    0,
-    0,
-    0,
-    0
-  );
-  if (dateInPath.error == null) {
+app.get("/api/:date?", (req, res) => {
+  const dateIntoNumber = Number(req.params.date);
+
+  if (req.params.date == null) {
+    const date = new Date();
     res.json({ unix: date.getTime(), utc: date.toUTCString() });
   } else {
-    res.json({ error: dateInPath.error });
+    if (Number.isNaN(dateIntoNumber)) {
+      let date = new Date(req.params.date);
+      if (date === "Invalid Date" || date.toUTCString() === "Invalid Date") {
+        res.json({ error: "Invalid Date" });
+      } else {
+        res.json({ unix: date.getTime(), utc: date.toUTCString() });
+      }
+    } else {
+      let date = new Date(dateIntoNumber);
+      res.json({ unix: date.getTime(), utc: date.toUTCString() });
+    }
   }
+  res.json({ error: "Invalid Date" });
 });
 
-// hostname/api/UNIX_TIMESTAMP
-app.get(/^\/api\/[1-9][0-9]{1,}\/?$/, (req, res) => {
-  const dateInPath = validateDateRoute(req.path);
-  const date = new Date(dateInPath.unix);
-
-  res.json({ unix: date.getTime(), utc: date.toUTCString() });
-});
-
-app.get("/api", (req, res) => {
-  const date = new Date();
-
-  res.json({ unix: date.getTime(), utc: date.toUTCString() });
-});
 // your first API endpoint...
 app.get("/api/hello", function(req, res) {
   res.json({ greeting: "hello API2" });
